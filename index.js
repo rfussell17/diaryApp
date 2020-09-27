892f3f8084620da7cf0866970acc200da47a908d
Robin Fussell <rfussell17@gmail.com>
2020-09-26 17:24:17 -0700

    styled text area

---
+++ index.js
@@ -0,0 +1,13 @@
+const entryForm = document.getElementById('entryForm');
+const entriesSection = document.getElementById('entries');
+const entryTextbox = document.getElementsByClassName('entry-textbox');
+
+function addEntryToDom(event) {
+    event.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox[0].value;
+    entriesSection.appendChild(entryDiv);
+}
+
+entryForm.addEventListener('submit', addEntryToDom)
