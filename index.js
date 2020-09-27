02794beed598a715bddcee1448709bff5142a719
Robin Fussell <rfussell17@gmail.com>
2020-09-27 13:15:35 -0700

    basic functionality and style finished

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ function addEntryToDom(event) {
   entriesNav.appendChild(displayEntryButton);
 
   displayEntryButton.addEventListener("click", function () {
-    const allEntries = document.querySelectorAll("single-entry");
+    const allEntries = document.querySelectorAll(".single-entry");
+    for(let i = 0; i < allEntries.length; i++) {
+      allEntries[i].style.display = 'none';
+    }
     entryDiv.style.display = "block";
   });
 
