08b9eb0c576e3c78e55436b16b0045e8763e6de0
Robin Fussell <rfussell17@gmail.com>
2020-09-26 17:42:01 -0700

    entry counter added

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const entryForm = document.getElementById('entryForm');
-const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector(".entries-nav");
+
+let count = 1;
 
 function addEntryToDom(event) {
-    event.preventDefault();
-    const entryDiv = document.createElement('div');
-    entryDiv.className = 'single-entry';
-    entryDiv.innerText = entryTextbox[0].value;
-    entriesSection.appendChild(entryDiv);
+  event.preventDefault();
+  const entryDiv = document.createElement('div');
+  entryDiv.className = 'single-entry';
+  entryDiv.innerText = entryTextbox.value;
+  entryDiv.style.display = 'none';
+
+  entriesSection.appendChild(entryDiv);
+  entryTextbox.value = '';
+
+  const displayEntryButton = document.createElement('button')
+  displayEntryButton.className = 'display-entry-button';
+  displayEntryButton.innerText = count
+  entriesNav.appendChild(displayEntryButton);
+
+  count++;
+
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
